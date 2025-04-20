a4b9a5052cbf4bd452398bbb18cf7a5915ee0e6b
brixdorf <itsromit@protonmail.com>
2025-04-20 18:09:00 +0530

    Created basic homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <h1 className='text-3xl underline'>Test</h1>
-    </>
-  )
+    <main className="relative min-h-screen">
+      <img
+        src="images/stadium.jpg"
+        alt="Football Stadium"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <h1 className="relative z-10 font-[Montserrat] text-white text-8xl font-extrabold pt-50 pl-10">
+        THE ULTIMATE HUB FOR FOOTBALL FANS
+      </h1>
+      <p className="relative z-10 font-[Montserrat] text-white text-2xl font-medium pl-10 max-w-4xl pt-5">
+        ClubZone is your all-in-one website to explore football clubs, discover
+        top players, and manage your personal favourites — all in one place.
+        Dive into club details, add players, and build your dream list.
+      </p>
+    </main>
+  );
 }
 
-export default App
+export default App;
