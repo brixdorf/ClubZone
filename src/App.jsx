8670ee62e7b147bdd56e864260f5d8ce6c4ef8ef
brixdorf <itsromit@protonmail.com>
2025-04-20 19:35:35 +0530

    Created and added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import "./App.css";
 import Button from "./components/Button";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main className="relative min-h-screen">
+      <div className="relative z-10 mb-0">
+        <Navbar/>
+      </div>
       <img
         src="images/stadium.jpg"
         alt="Football Stadium"
         className="absolute inset-0 w-full h-full object-cover"
       />
-      <h1 className="relative z-10 font-[Montserrat] text-white text-8xl font-extrabold pt-50 pl-10">
+      <h1 className="relative z-10 font-[Montserrat] text-white text-8xl font-extrabold pt-30 pl-10">
         THE ULTIMATE HUB FOR FOOTBALL FANS
       </h1>
       <p className="relative z-10 font-[Montserrat] text-white text-2xl font-medium pl-10 max-w-4xl pt-5">
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-black">
+      <div className="px-4 py-2 grid grid-cols-3 items-center">
+        <div>
+          <Link to="/">
+            <img
+              src="/images/white_logo.svg"
+              alt="ClubZone Logo"
+              className="h-15 w-auto"
+            />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center space-x-10">
+          <Link
+            to="/"
+            className="font-[Montserrat] text-white hover:text-gray-300 transition duration-300 relative group"
+          >
+            HOME
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-300"></span>
+          </Link>
+          <Link
+            to="/clubs"
+            className="font-[Montserrat] text-white hover:text-gray-300 transition duration-300 relative group"
+          >
+            CLUBS
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-300"></span>
+          </Link>
+          <Link
+            to="/favorites"
+            className="font-[Montserrat] text-white hover:text-gray-300 transition duration-300 relative group"
+          >
+            FAVOURITES
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-300"></span>
+          </Link>
+          <Link
+            to="/add-player"
+            className="font-[Montserrat] text-white hover:text-gray-300 transition duration-300 relative group"
+          >
+            ADD PLAYER
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-300"></span>
+          </Link>
+        </div>
+        <div></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
